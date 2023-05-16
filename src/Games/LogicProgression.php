1fdd3204693d10535dc52f7db6dd3d32eff1a85d
Svetlana Ostritsova <ostritsova@mail.ru>
2023-05-16 21:25:01 +0300

    bugs fixed standard=PSR12

---
+++ src/Games/LogicProgression.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace BrainGames\Games\LogicProgression;
+
+use function cli\line;
+use function cli\prompt;
+
+function &calculateProgression()
+{
+    $begin = mt_rand(0, 10) ;
+    $difference = mt_rand(0, 10) ;
+    $array = [] ;
+    $a = $begin ;
+    $d = $difference ;
+    $array[0] = $a ;
+
+    for ($i = 1; $i <= 10; $i++) {
+        $a = $a + $d ;
+        $array[$i] = $a ;
+    }
+
+//print_r($array) ;
+    return($array) ;
+}
+
+function runProgression()
+{
+    //Приветствие
+
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('What number is missing in the progression?');
+
+    //Вопрос-ответ
+    $j = 1 ;
+    $stepToVin = 3 ;
+    while ($j <= $stepToVin) {
+        $newArray =& calculateProgression() ;
+        $hidden = mt_rand(0, 10) ;
+        $i = $hidden ;
+        $answer = $newArray[$i] ;
+        $newArray[$i] = '..' ;
+        $string = implode(' ', $newArray) ;
+        $correctAnswer = $answer;
+       // $question = $string ;
+        line("Question: %s", $string);
+        $playerAnswer = prompt('Your answer');
+        if ($playerAnswer != $correctAnswer) {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $playerAnswer, $correctAnswer);
+            line("Let's try again, %s", $name);
+            break;
+        }
+        line("Correct!");
+        $j++;
+    }
+    line("Congratulations, %s!", $name);
+}
