a15fad0328830a7535c75f517d271229f4b3055a
Svetlana Ostritsova <ostritsova@mail.ru>
2023-05-25 22:31:26 +0300

    created src/Games/LogicPrime.php

---
+++ src/Games/LogicPrime.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BrainGames\Games\LogicPrime;
+
+use function cli\line;
+use function cli\prompt;
+
+//Проверка простое ли число
+function isPrime($value)
+{
+    $num = $value;
+    $flag = true;
+    for ($i = 2; $i < $num; $i++) {
+        if ($num % $i === 0) {
+            $flag = false;
+            break; // выйдем из цикла
+        }
+    }
+
+    return $flag ;
+}
+
+function runPrime()
+{
+    //Приветствие
+
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+    //Вопрос-ответ
+    $correctAnswer = '';
+    $i = 1;
+    $stepToVin = 3;
+
+    while ($i <= $stepToVin) {
+        $question = random_int(0, 100);
+        isPrime($question) ? $correctAnswer = 'yes' : $correctAnswer = 'no';
+        line('Question: %s', $question);
+        $playerAnswer = prompt('Your answer');
+        if ($playerAnswer != $correctAnswer) {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $playerAnswer, $correctAnswer);
+            line("Let's try again, %s", $name);
+            break;
+        }
+        line("Correct!");
+        $i++;
+    }
+    line("Congratulations, %s!", $name);
+}
