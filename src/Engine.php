9a3a2e0458a8b39e8a6edcc198d2d96bec3e1eee
Svetlana Ostritsova <ostritsova@mail.ru>
2023-06-05 01:07:46 +0300

    general logic moved to the engine for brain-calc

--- src/Engine.php
+++ src/Engine.php
@@ -1,1 +1,35 @@
 <?php
+
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function runGame(string $line, $callback) {
+
+    //Приветствие
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line("%s", $line) ;
+    
+    //Вопрос-ответ
+   
+    for($i = 1; $i < 3; $i++){
+        $arrayLogic = call_user_func($callback);
+        line("Question: %s" , $arrayLogic['question']);
+        $playerAnswer = prompt('Your answer');
+        if ($playerAnswer != $arrayLogic['correctAnswer']) {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.",
+            $playerAnswer, $arrayLogic['correctAnswer']);
+            line("Let's try again, %s", $name);
+           break;
+        } else {
+            line("Correct!");
+                if($i = 3) {
+                    line("Congratulations, %s!", $name);                                     
+                }
+        }
+    }
+    return;    
+}
